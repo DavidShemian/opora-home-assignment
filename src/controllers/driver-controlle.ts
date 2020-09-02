d676302dbb839b51e2aee1da81b439e3a5973cff
David Shemian <davidshemian18@gmail.com>
2020-09-02 10:44:21 +0300

    added prettierrc

--- src/controllers/driver-controlle.ts
+++ src/controllers/driver-controlle.ts
@@ -1,55 +1,44 @@
-import { DriverService } from "./../services/driver-service";
-import { Router, Response, Request, NextFunction } from "express";
+import { DriverService } from './../services/driver-service';
+import { Router, Response, Request, NextFunction } from 'express';
 
 export class DriverController {
-  get router(): Router {
-    return this._router;
-  }
-
-  private _router: Router;
-  private driverService: DriverService;
-  private path = "/drivers";
-
-  constructor(router: Router) {
-    this._router = router;
-    this.driverService = new DriverService();
-    this.initializeRoutes();
-  }
-
-  private getCurrentSessionDriversSortedByWins = async (
-    _request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const drivers = await this.driverService.getCurrentSessionDriversSortedByWins();
-      response.status(200).json(drivers);
-    } catch (e) {
-      next(e);
+    get router(): Router {
+        return this._router;
     }
-  };
 
-  private getDriverRaces = async (
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const { driverId } = request.params;
+    private _router: Router;
+    private driverService: DriverService;
+    private path = '/drivers';
 
-      const drivers = await this.driverService.getDriverRaces(+driverId);
-
-      response.status(200).json(drivers);
-    } catch (e) {
-      next(e);
+    constructor(router: Router) {
+        this._router = router;
+        this.driverService = new DriverService();
+        this.initializeRoutes();
     }
-  };
 
-  private initializeRoutes() {
-    this._router.get(
-      `${this.path}/current-season`,
-      this.getCurrentSessionDriversSortedByWins
-    );
-    this._router.get(`${this.path}/:driverId(\\d+)/races`, this.getDriverRaces);
-  }
+    private getCurrentSessionDriversSortedByWins = async (_request: Request, response: Response, next: NextFunction) => {
+        try {
+            const drivers = await this.driverService.getCurrentSessionDriversSortedByWins();
+            response.status(200).json(drivers);
+        } catch (e) {
+            next(e);
+        }
+    };
+
+    private getDriverRaces = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            const { driverId } = request.params;
+
+            const drivers = await this.driverService.getDriverRaces(+driverId);
+
+            response.status(200).json(drivers);
+        } catch (e) {
+            next(e);
+        }
+    };
+
+    private initializeRoutes() {
+        this._router.get(`${this.path}/current-season`, this.getCurrentSessionDriversSortedByWins);
+        this._router.get(`${this.path}/:driverId(\\d+)/races`, this.getDriverRaces);
+    }
 }
